ded18b2d28f8848fcba7a8791c1e876f6568ca21
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2021-03-19 21:57:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,29 +5,30 @@ const nodemailer = require("nodemailer");
 admin.initializeApp();
 require("dotenv").config();
 
-const {SENDER_EMAIL, SENDER_PASSWORD} = process.env;
+const { SENDER_EMAIL, SENDER_PASSWORD } = process.env;
 
 exports.sendEmailNotification = functions.database
-.ref("/demand-private")
-.onCreate((snap, ctx) => {
-const data = snap.data();
-const authData = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: SENDER_EMAIL,
-    pass: SENDER_PASSWORD,
-  },
-});
-authData
-.sendMail({
-from: "developer@seachvaccines.com",
-to: `${data.email}`,
-subject: "Vaccines added successfully",
-text: "searchvaccines",
-html: "searchvaccines",
-})
-.then((res) => console.log("email sent"))
-.catch((e) => console.log(e));
-});
+  .ref("/demand-private/{docId}")
+  .onCreate((snap, ctx) => {
+    const data = snap.val();
+    console.log(snap);
+    const authData = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: SENDER_EMAIL,
+        pass: SENDER_PASSWORD,
+      },
+    });
+    authData
+      .sendMail({
+        from: "developer@seachvaccines.com",
+        to: data ? data.email : "saiashish7777@gmail.com",
+        subject: "Vaccines added successfully",
+        text: data ? "searchvaccines" : "error",
+        html: data ? "searchvaccines" : "error",
+      })
+      .then((res) => console.log("email sent"))
+      .catch((e) => console.log(e));
+  });
